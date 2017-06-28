b2d22e2f7cc77bbd6cc2d405b9d8ac5f674e5aa2
Stephen Checkoway <s@pahtak.org>
2017-06-28 18:02:37 -0500

    Ita, not lta.

--- getfonts.sh
+++ getfonts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Download the .otf font files from github.
-for font in Bold Boldlta Heavy Light Lightlta Medium Regular Regularlta Thin UltraLight; do
+for font in Bold BoldIta Heavy Light LightIta Medium Regular RegularIta Thin UltraLight; do
 	curl -LO "https://github.com/BrooklynResponsive/Purpose/raw/master/legacy/fonts/Theinhardt-${font}.otf"
 done
