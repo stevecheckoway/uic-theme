da43eb53bda0e90d12e68933701b17e07e55a422
Stephen Checkoway <s@pahtak.org>
2017-06-28 18:26:02 -0500

    Get them all.

--- README.md
+++ README.md
@@ -3,6 +3,4 @@ Simple Keynote theme using the Theinhardt fonts specified by the [UIC brand
 binder](https://marketing.uic.edu/files/2015/11/brand-binder_final.pdf).
 
 # Fonts
-The Theinhardt font can be downloaded
-[here](https://github.com/BrooklynResponsive/Purpose/tree/master/legacy/fonts)
-or using the `getfonts.sh` script.
+The Theinhardt font can be downloaded from github using the `getfonts.sh` script.
--- getfonts.sh
+++ getfonts.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
 # Download the .otf font files from github.
-for font in Bold BoldIta Heavy Light LightIta Medium Regular RegularIta Thin UltraLight; do
-	curl -LO "https://github.com/BrooklynResponsive/Purpose/raw/master/legacy/fonts/Theinhardt-${font}.otf"
-done
+
+set -e
+
+url='https://github.com/joshuarrrr/ieee-spectrum-charts/raw/96b0da3b5c8e3fa92ff82fe3456efdeefaf9b35a/app/fonts/Theinhardt'
+fonts='Black,Bold,Hairline,Heavy,Light,Medium,Regular,Thin,Ultralight'
+
+curl --location \
+	--progress-bar \
+	--create-dirs \
+	--output 'Theinhardt/Theinhardt-#1#2.otf' \
+	"${url}/Theinhardt-{${fonts}}{,Ita}.otf"
